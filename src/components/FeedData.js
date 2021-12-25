af445564b05f5e46856872c24d4d4efac35cbd63
theinfosecguy <keshavaarav22@gmail.com>
2021-12-25 19:26:28 +0530

    NASA NEO Feed API Data Setup

--- src/components/FeedData.js
+++ src/components/FeedData.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Accordion from 'react-bootstrap/Accordion';
 
 const FeedData = (props) => {
     const {data} = props;
@@ -6,27 +7,33 @@ const FeedData = (props) => {
 
     return (
         <div>
-            {
-                Object.keys(near_earth_objects).map((key, index) => {
-                    return (
-                        <div key={index}>
-                            <h1>Date: {key}</h1>
-                            {
-                                near_earth_objects[key].map((item, index) => {
-                                    return (
-                                        <div key={index}>
-                                            <h4>Name: {item.name}</h4>
-                                            <h5>JPL URL: {item.nasa_jpl_url}</h5>
-                                            <h5>Designation: {item.designation}</h5>
-                                            <h5>Minimum Diameter: {item.estimated_diameter.meters.estimated_diameter_min}</h5>
-                                            <h5>Maximum Diameter: {item.estimated_diameter.meters.estimated_diameter_max}</h5>
-                                        </div>
-                                    )
-                                })
-                            }
-                        </div>
-                )})
-            }
+            <Accordion defaultActiveKey="0">
+                {   
+                    Object.keys(near_earth_objects).map((key, index) => {
+                        return (
+                            <Accordion.Item eventKey={index} className="mt-3">
+                                <Accordion.Header>
+                                    {key}
+                                </Accordion.Header>
+                                <Accordion.Body>
+                                {
+                                    near_earth_objects[key].map((item, index) => {
+                                        return (
+                                            <div key={index}>
+                                                <h4>Name: {item.name}</h4>
+                                                <h5>JPL URL: {item.nasa_jpl_url}</h5>
+                                                <h5>Designation: {item.designation}</h5>
+                                                <h5>Minimum Diameter: {item.estimated_diameter.meters.estimated_diameter_min}</h5>
+                                                <h5>Maximum Diameter: {item.estimated_diameter.meters.estimated_diameter_max}</h5>
+                                            </div>
+                                        )
+                                    })
+                                }
+                                </Accordion.Body>
+                            </Accordion.Item>
+                    )})
+                }
+            </Accordion>
         </div>
     )
 }
